a3ddc48c3d359eb289c1d397609486fc3bbbd81c
japhheth.onyenuwe <japheth.onyenuwe@nomba.com>
2023-11-16 13:01:31 +0100

    initial-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { LayoutProp } from "@/app/lib/definitions";
+
+const RootLayout = ({ children }: LayoutProp) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
