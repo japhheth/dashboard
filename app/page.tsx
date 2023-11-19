a626b806f14d8413ca55c03d74fc81e412ab00f9
japhheth.onyenuwe <japheth.onyenuwe@nomba.com>
2023-11-19 11:20:17 +0100

    api setup and dashboard ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
-
 import Link from "next/link";
-export default function Page() {
+
+const Page = () => {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -53,4 +53,6 @@ export default function Page() {
       </div>
     </main>
   );
-}
+};
+
+export default Page;
